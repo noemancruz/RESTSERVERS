9e7c61cad26062b4820f9d60d1903b3a3960f356
Noeman <noemancruz@gmail.com>
2018-10-14 02:14:27 -0500

    probando2

--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-//require('./config/config');
+require('./config/config2');
 const mongoose = require('mongoose');
 const app = express();
 
 app.use(require('./routes/usuarios'));
 
 //parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
-    //parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+//parse application/json
+app.use(bodyParser.json());
 
 
 mongoose.connect('mongodb://localhost:27017/cafe', (err, res) => {
